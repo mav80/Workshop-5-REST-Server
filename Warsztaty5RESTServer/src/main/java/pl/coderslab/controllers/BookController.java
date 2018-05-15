8c9100a0a59cffa627361d6bfdfa8d93c03f25c3
“mav80” <mav80@prokonto.pl>
2018-05-15 18:39:57 +0200

    Add Jackosn library to configuration file for easier JSON support, added another test controller.

--- Warsztaty5RESTServer/src/main/java/pl/coderslab/controllers/BookController.java
+++ Warsztaty5RESTServer/src/main/java/pl/coderslab/controllers/BookController.java
@@ -2,13 +2,23 @@ package pl.coderslab.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import pl.coderslab.app.Book;
 
 @Controller
 public class BookController {
 	
 	@RequestMapping("/hello")
+	@ResponseBody
 	public String hello(){
 	return "{hello: World}";
+	}
+	
+	@RequestMapping("/helloBook")
+	@ResponseBody
+	public Book helloBook(){
+	return new Book(1L,"9788324631766","Thiniking in Java",	"Bruce Eckel","Helion","programming");
 	}
 
 }
