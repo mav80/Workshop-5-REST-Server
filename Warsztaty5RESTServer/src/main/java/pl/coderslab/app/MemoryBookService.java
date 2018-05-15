a221a962de3b2f23a2e93dc03671799d56917b39
“mav80” <mav80@prokonto.pl>
2018-05-15 20:04:40 +0200

    Add book service class with some book examples.

---
+++ Warsztaty5RESTServer/src/main/java/pl/coderslab/app/MemoryBookService.java
@@ -0,0 +1,30 @@
+package pl.coderslab.app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemoryBookService {
+	
+	private List<Book> list;
+	
+	public MemoryBookService() {
+		
+	list = new ArrayList<>();
+	list.add(new Book(1L, "8854433", "Wampir z MO", "Andrzej Pilipiuk",	"Fabryka Słów", "science-fiction"));
+	list.add(new Book(2L, "656857", "Szczury Wrocławia", "Robert J. Szmidt", "Helion", "horror"));
+	list.add(new Book(3L, "24785332", "Cyfrowa twierdza", "Dan Brown", "Amber", "thriller"));
+	list.add(new Book(4L, "57454356", "Księga jesiennych demonów", "Jarosław Grzędowicz", "Fabryka Słów", "horror"));
+	}
+	
+	public List<Book> getList() {
+		return list;
+		}
+	
+	public void setList(List<Book> list) {
+		this.list = list;
+		}
+
+}
--- Warsztaty5RESTServer/src/main/java/pl/coderslab/controllers/BookController.java
+++ Warsztaty5RESTServer/src/main/java/pl/coderslab/controllers/BookController.java
@@ -1,14 +1,26 @@
 package pl.coderslab.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import pl.coderslab.app.Book;
+import pl.coderslab.app.MemoryBookService;
 
 @Controller
 public class BookController {
 	
+	public MemoryBookService memoryBookService;
+	
+	@Autowired
+	public BookController(MemoryBookService memoryBookService) {
+		this.memoryBookService = memoryBookService;
+	}
+
+	
+	
+	//first two test controllers
 	@RequestMapping("/hello")
 	@ResponseBody
 	public String hello(){
